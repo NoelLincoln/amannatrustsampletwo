64082bf8d01e91e15e08139b3ab7aed550699f61
Noel <noelsobryant@gmail.com>
2023-02-16 14:41:45 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,488 @@
+<header className="continer-fluid ">
+<div  className="header-top">
+<div className="container">
+<div className="row col-det">
+   <div className="col-lg-6 d-none d-lg-block">
+       <ul className="ulleft">
+           <li>
+               <i className="far fa-envelope"></i>
+               sales@smarteyeapps.com
+               <span>|</span></li>
+           <li>
+               <i className="fas fa-phone-volume"></i>
+               +876 987 666 5433</li>
+       </ul>
+   </div>
+   <div className="col-lg-3 col-md-6 folouws">
+     
+       <ul className="ulright">
+          <li> <small>Folow Us </small>:</li>
+           <li>
+               <i className="fab fa-facebook-square"></i>
+           </li>
+           <li>
+               <i className="fab fa-twitter-square"></i>
+           </li>
+           <li>
+               <i className="fab fa-instagram"></i>
+           </li>
+           <li>
+               <i className="fab fa-linkedin"></i>
+           </li>
+       </ul>
+   </div>
+   <div className="col-lg-3 d-none d-md-block col-md-6 btn-bhed">
+       <button className="btn btn-sm btn-success">Join Us</button>
+       <button className="btn btn-sm btn-default">Donate</button>
+   </div>
+</div>
+</div>
+</div>
+<div id="menu-jk" className="header-bottom">
+<div className="container">
+<div className="row nav-row">
+   <div className="col-lg-3 col-md-12 logo">
+       <a href="index.html">
+           <img src="assets/images/logo.jpg" alt=""/>
+           <a data-toggle="collapse" data-target="#menu" href="#menu"><i className="fas d-block d-lg-none  small-menu fa-bars"></i></a>
+       </a>
+
+   </div>
+   <div id="menu" className="col-lg-9 col-md-12 d-none d-lg-block nav-col">
+
+               <ul className="navbad">
+                   <li className="nav-item active">
+                       <a className="nav-link" href="index.html">Home
+                       </a>
+                   </li>
+                   <li className="nav-item">
+                       <a className="nav-link" href="about_us.html">About Us</a>
+                   </li>
+                   <li className="nav-item">
+                       <a className="nav-link" href="services.html">Services</a>
+                   </li>
+
+                   <li className="nav-item">
+                       <a className="nav-link" href="gallery.html">Gallery</a>
+                   </li>
+
+                   <li className="nav-item">
+                       <a className="nav-link" href="blog.html">Blog</a>
+                   </li>
+                   <li className="nav-item">
+                       <a className="nav-link" href="contact_us.html">Contact US</a>
+                   </li>
+
+
+
+
+               </ul>
+
+
+   </div>
+</div>
+</div>
+</div> 
+</header>
+
+    
+{/* Slider Starts Here  */}
+<div className="slider">
+<div className="owl-carousel ">
+<div className="slider-img">
+   <div className="item">
+       <div className="slider-img"><img src="assets/images/slider/slider-3.jpg" alt=""/></div>
+       <div className="container">
+           <div className="row">
+               <div className="col-lg-offset-2 col-lg-8 col-md-offset-2 col-md-8 col-sm-12 col-xs-12">
+                   <div className="animated bounceInDown slider-captions">
+                       <h1 className="slider-title">Most Stylish Free Travel Website</h1>
+                       <p className="slider-text hidden-xs">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+                       
+                   </div>
+               </div>
+           </div>
+       </div>
+   </div>
+</div>
+<div className="item">
+   <div className="slider-img"><img src="assets/images/slider/slider-1.jpg" alt=""/></div>
+   <div className="container">
+       <div className="row">
+           <div className="col-lg-offset-2 col-lg-8 col-md-offset-2 col-md-8 col-sm-12 col-xs-12">
+               <div className="slider-captions ">
+                   <h1 className="slider-title">It's time for better help.</h1>
+                   <p className="slider-text hidden-xs">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+                   
+               </div>
+           </div>
+       </div>
+   </div>
+</div>
+<div className="item">
+   <div className="slider-img"><img src="assets/images/slider/slider-2.jpg" alt=""/></div>
+   <div className="container">
+       <div className="row">
+           <div className="col-lg-offset-2 col-lg-8 col-md-offset-2 col-md-8 col-sm-12 col-xs-12">
+               <div className="slider-captions ">
+                   <h1 className="slider-title">Most Attractive Travel Template</h1>
+                   <p className="slider-text hidden-xs">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+                  
+               </div>
+           </div>
+       </div>
+   </div>
+</div>
+
+</div>
+</div>                 
+     
+                               {/* About Us */}
+
+<div className="about-us container-fluid">
+<div className="container">
+
+<div className="row natur-row no-margin w-100">
+<div className="text-part col-md-6">
+   <h2>About Our Charity</h2>
+   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis varius faucibus
+       ligula non congue. Suspendisse at pretium massa, sit amet
+       vulputate nibh. Nam posuere eros dolor. Donec vel arcu sagittis, pretium nisl </p>
+   <p> Cras faucibus laoreet nibh, sit amet tincidunt leo mollis in. Etiam eu mauris metus.
+       Nulla facilisi. Etiam vestibulum,
+       nisi et convallis elementum, leo orci aliquam metus, id posuere massa neque vitae
+       arcu.</p>
+
+   <p>Integer vulputate vehicula dolor a eleifend. Duis aliquam condimentum sapien,
+       eget tempor justo. Aenean porttitor nibh metus, sollicitudin egestas metus posuere et
+       . Fusce egestas volutpat metus, in sodales sem bibendum porta. Nunc hendrerit nunc sit
+       amet tellus posuere, at malesuada sem gravida. Integer maximus ultricies augue, at
+       dapibus elit bibendum consequat. Cras faucibus tellus eleifend, fermentum purus in,
+       dapibus sapien. Praesent nec ornare risus. Etiam iaculis, ligula vel gravida
+       vestibulum, urna justo posuere ante,
+       id pretium massa arcu sed mi. Nunc a sollicitudin sem. Duis tempus </p>
+</div>
+<div className="image-part col-md-6">
+   <div className="about-quick-box row">
+       <div className="col-md-6">
+           <div className="about-qcard">
+              <i className="fas fa-user"></i>
+               <p>Becom a Volunteer</p>
+           </div>
+       </div>
+        <div className="col-md-6">
+           <div className="about-qcard ">
+              <i className="fas fa-search-dollar red"></i>
+               <p>Quick Fundrais</p>
+           </div>
+       </div>
+        <div className="col-md-6">
+           <div className="about-qcard ">
+              <i className="fas fa-donate yell"></i>
+               <p>Giv Donation</p>
+           </div>
+       </div>
+        <div className="col-md-6">
+           <div className="about-qcard ">
+              <i className="fas fa-hands-helping blu"></i>
+               <p>Help Someone</p>
+           </div>
+       </div>
+   </div>
+</div>
+</div>
+</div>
+</div>     
+
+
+
+{/* Mission Vision Start Here  */}
+
+<section className="container-fluid mission-vision">
+<div className="container">
+<div className="row mission">
+ <div className="col-md-6 mv-det">
+     <h1>Our Mission</h1>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer neque libero, pulvinar et elementum quis, facilisis eu ante. Mauris non placerat sapien. Pellentesque tempor arcu non odio scelerisque ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam varius eros consequat auctor gravida. Fusce tristique lacus at urna sollicitudin pulvinar. Suspendisse hendrerit ultrices mauris.</p>
+ </div>
+ <div className="col-md-6 mv-img">
+     <img src="assets/images/misin.jpg" alt=""/>
+ </div>
+</div>
+<div className="row vision">
+ <div className="col-md-6 mv-img">
+      <img src="assets/images/vision.jpg" alt=""/>
+ </div>
+ <div className="col-md-6 mv-det">
+      <h1>Our Vision</h1>
+      <p>Ut ultricies lacus a rutrum mollis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed porta dolor quis felis pulvinar dignissim. Etiam nisl ligula, ullamcorper non metus vitae, maximus efficitur mi. Vivamus ut ex ullamcorper, scelerisque lacus nec, commodo dui. Proin massa urna, volutpat vel augue eget, iaculis tristique dui.</p>
+ </div>
+</div>
+</div>
+</section>   
+
+
+
+{/* Events Start Here */}
+<section className="events">
+<div className="container">
+<div className="session-title row">
+   <h2>Popular Causes</h2>
+   <p>We are a non-profital & Charity raising money for child education</p> 
+</div>
+<div className="event-ro row">
+   <div className="col-md-4 col-sm-6">
+       <div className="event-box">
+           <img src="assets/images/events/image_08.jpg" alt=""/>
+           <h4>Child Education in Africa</h4>
+           
+           <p className="raises"><span>Raised : $34,425</span> / $500,000 </p>
+           <p className="desic">Lorem Ipsum is simply dummy text of the printing and typesetting industry.  Lorem Ipsum has been the industry's </p>
+           <button className="btn btn-success btn-sm">Donate Now</button>
+       </div>
+   </div>
+   <div className="col-md-4 col-sm-6">
+       <div className="event-box">
+           <img src="assets/images/events/image_06.jpg" alt=""/>
+           <h4>Child Education in Africa</h4>
+           <p className="raises"><span>Raised : $34,425</span> / $500,000 </p>
+           <p className="desic">Lorem Ipsum is simply dummy text of the printing and typesetting industry.  Lorem Ipsum has been the industry's </p>
+           <button className="btn btn-success btn-sm">Donate Now</button>
+       </div>
+   </div>
+   <div className="col-md-4 col-sm-6">
+       <div className="event-box">
+           <img src="assets/images/events/image_04.jpg" alt=""/>
+           <h4>Child Education in Africa</h4>
+           <p className="raises"><span>Raised : $34,425</span> / $500,000 </p>
+           <p className="desic">Lorem Ipsum is simply dummy text of the printing and typesetting industry.  Lorem Ipsum has been the industry's </p>
+           <button className="btn btn-success btn-sm">Donate Now</button>
+       </div>
+   </div>
+</div>
+</div>
+</section>   
+
+
+
+
+{/*    Charity Number Starts Here */}
+
+<div className="doctor-message">
+<div className="inner-lay">
+<div className="container">
+  <div className="row session-title">
+      <h2>Our Achievemtns in Numbers</h2>
+      <p>We can talk for a long time about advantages of our Dental clinic before other medical treatment facilities.
+But you can read the following facts in order to make sure of all pluses of our clinic:</p>
+  </div>
+   <div className="row">
+       <div className="col-sm-3 numb">
+           <h3>12+</h3>
+           <span>YEARS OF EXPEREANCE</span>
+       </div>
+       <div className="col-sm-3 numb">
+           <h3>1812+</h3>
+           <span>HAPPY CHILDRENS</span>
+       </div>
+       <div className="col-sm-3 numb">
+           <h3>52+</h3>
+           <span>EVENTS</span>
+       </div>
+       <div className="col-sm-3 numb">
+           <h3>48+</h3>
+           <span>FUNT RAISED</span>
+       </div>
+   </div>
+</div>
+
+</div>
+
+</div> 
+
+{/* Our Team Starts Here  */}
+<section className="our-team team-11">
+<div className="container">
+   <div className="session-title row">
+         <h2>Meet our Team</h2>
+         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fringilla vel nisl a dictum. Donec ut est arcu. Donec hendrerit velit</p>
+   </div>
+   <div className="row team-row">
+       <div className="col-md-3 col-sm-6">
+           <div className="single-usr">
+               <img src="assets/images/team/team-memb1.jpg" alt=""/>
+               <div className="det-o">
+                   <h4>David Kanuel</h4>
+                   <i>CEO </i>
+               </div>
+           </div>
+       </div>
+       <div className="col-md-3 col-sm-6">
+           <div className="single-usr">
+               <img src="assets/images/team/team-memb2.jpg" alt=""/>
+               <div className="det-o">
+                   <h4>David Kanuel</h4>
+                   <i>CFO</i>
+               </div>
+           </div>
+       </div>
+       <div className="col-md-3 col-sm-6">
+           <div className="single-usr">
+               <img src="assets/images/team/team-memb3.jpg" alt=""/>
+               <div className="det-o">
+                   <h4>David Kanuel</h4>
+                   <i>Team Leader</i>
+               </div>
+           </div>
+       </div>
+       <div className="col-md-3 col-sm-6">
+           <div className="single-usr">
+               <img src="assets/images/team/team-memb4.jpg" alt=""/>
+               <div className="det-o">
+                   <h4>David Kanuel</h4>
+                   <i>Project Manager</i>
+               </div>
+           </div>
+       </div>
+
+
+   </div>
+</div>
+</section>      
+
+
+
+{/* Our Blog Starts  */}
+
+<section className="our-blog">
+<div className="container">
+    <div className="row session-title">
+       <h2> Our Blog </h2>
+       <p>Take a look at what people say about US </p>
+   </div>
+   <div className="blog-row row">
+       <div className="col-md-4 col-sm-6">
+           <div className="single-blog">
+               <figure>
+                   <img src="assets/images/events/image_01.jpg" alt=""/>
+               </figure>
+               <div className="blog-detail">
+                   <small>By Admin | August 10 2018</small>
+                   <h4>Methods of Recuirtments</h4>
+                   <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam justo neque, aliquet sit amet elementum vel, vehicula eget eros. Vivamus arcu metus, mattis sed sagittis at, sagittis quis neque. Praesent.</p>
+                   <div className="link">
+                       <a href="">Read more </a><i className="fas fa-long-arrow-alt-right"></i>
+                   </div>
+               </div>
+               
+               
+           </div>
+       </div>
+       <div className="col-md-4 col-sm-6">
+           <div className="single-blog">
+               <figure>
+                   <img src="assets/images/events/image_02.jpg" alt=""/>
+               </figure>
+               <div className="blog-detail">
+                   <small>By Admin | August 10 2018</small>
+                   <h4>Methods of Recuirtments</h4>
+                   <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam justo neque, aliquet sit amet elementum vel, vehicula eget eros. Vivamus arcu metus, mattis sed sagittis at, sagittis quis neque. Praesent.</p>
+                   <div className="link">
+                       <a href="">Read more </a><i className="fas fa-long-arrow-alt-right"></i>
+                   </div>
+               </div>
+               
+               
+           </div>
+       </div>
+       <div className="col-md-4 col-sm-6">
+           <div className="single-blog">
+               <figure>
+                   <img src="assets/images/events/image_03.jpg" alt=""/>
+               </figure>
+               <div className="blog-detail">
+                   <small>By Admin | August 10 2018</small>
+                   <h4>Methods of Recuirtments</h4>
+                   <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam justo neque, aliquet sit amet elementum vel, vehicula eget eros. Vivamus arcu metus, mattis sed sagittis at, sagittis quis neque. Praesent.</p>
+                   <div className="link">
+                       <a href="">Read more </a><i className="fas fa-long-arrow-alt-right"></i>
+                   </div>
+               </div>
+               
+               
+           </div>
+       </div>
+   </div>
+</div>
+</section>
+
+<footer className="footer">
+<div className="container">
+<div className="row">
+   <div className="col-md-4 col-sm-12">
+       <h2>About Us</h2>
+       <p>
+           Smart Eye is a leading provider of information technology, consulting, and business process services. Our dedicated employees offer strategic insights, technological expertise and industry experience.
+       </p>
+       <p>We focus on technologies that promise to reduce costs, streamline processes and speed time-to-market, Backed by our strong quality processes and rich experience managing global... </p>
+   </div>
+   <div className="col-md-4 col-sm-12">
+       <h2>Useful Links</h2>
+       <ul className="list-unstyled link-list">
+           <li><a ui-sref="about" href="#/about">About us</a><i className="fa fa-angle-right"></i></li>
+           <li><a ui-sref="portfolio" href="#/portfolio">Portfolio</a><i className="fa fa-angle-right"></i></li>
+           <li><a ui-sref="products" href="#/products">Latest jobs</a><i className="fa fa-angle-right"></i></li>
+           <li><a ui-sref="gallery" href="#/gallery">Gallery</a><i className="fa fa-angle-right"></i></li>
+           <li><a ui-sref="contact" href="#/contact">Contact us</a><i className="fa fa-angle-right"></i></li>
+       </ul>
+   </div>
+   <div className="col-md-4 col-sm-12 map-img">
+       <h2>Contact Us</h2>
+       <address className="md-margin-bottom-40">
+           BlueDart <br>
+           Marthandam (K.K District) <br>
+           Tamil Nadu, IND <br>
+           Phone: +91 9159669599 <br>
+           Email: <a href="mailto:info@anybiz.com" className="">info@bluedart.in</a><br>
+           Web: <a href="smart-eye.html" className="">www.bluedart.in</a>
+       </address>
+
+   </div>
+</div>
+
+
+<div className="nav-box row clearfix">
+   <div className="inner col-md-9 clearfix">
+       <ul className="footer-nav clearfix">
+           <li><a href="#">Home</a></li>
+           <li><a href="#">About</a></li>
+           <li><a href="#">Gallery</a></li>
+           <li><a href="#">Servies</a></li>
+           <li><a href="#">Blog</a></li>
+           <li><a href="#">Contact</a></li>
+       </ul>
+
+     
+   </div>
+     <div className="donate-link col-md-3"><a href="donate.html" className="btn btn-primary "><span className="btn-title">Donate Now</span></a></div>
+</div>
+
+</div>
+
+
+</footer>
+
+<div className="copy">
+<div className="container">
+   <a href="https://www.smarteyeapps.com/">2015 &copy; All Rights Reserved | Designed and Developed by Smarteyeapps</a>
+   
+   <span>
+   <a><i className="fab fa-github"></i></a>
+   <a><i className="fab fa-google-plus-g"></i></a>
+   <a><i className="fab fa-pinterest-p"></i></a>
+   <a><i className="fab fa-twitter"></i></a>
+   <a><i className="fab fa-facebook-f"></i></a>
+</span>
+</div>
+
+</div>
